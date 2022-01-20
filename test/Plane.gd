af86c25712ede331ea4f2cced64ee1d00eb33a37
Valeron <32789691+Valeron-T@users.noreply.github.com>
2022-01-20 14:06:37 +0530

    Add files via upload

--- test/Plane.gd
+++ test/Plane.gd
@@ -1,26 +1,12 @@
 extends KinematicBody
 
-# Can't fly below this speed
-var min_flight_speed = 5
-# Maximum airspeed
-var max_flight_speed = 100
+var flight_speed = 10
 # Turn rate
 var turn_speed = 0.75
 # Climb/dive rate
 var pitch_speed = 0.8
 # Wings "autolevel" speed
 var level_speed = 2.0
-# Throttle change speed
-var throttle_delta = 30
-# Acceleration/deceleration
-var acceleration = 6.0
-
-# Current speed
-var forward_speed = 0
-# Throttle input speed
-var target_speed = 0
-# Lets us change behavior when grounded
-var grounded = false
 
 var velocity = Vector3.ZERO
 var turn_input = 0
@@ -28,12 +14,6 @@ var pitch_input = 0
 
 
 func get_input(delta):
-	# Throttle input
-	if Input.is_action_pressed("throttle_up"):
-		target_speed = min(forward_speed + throttle_delta * delta, max_flight_speed)
-	if Input.is_action_pressed("throttle_down"):
-		var limit = 0 if grounded else min_flight_speed
-		target_speed = max(forward_speed - throttle_delta * delta, limit)
 	# Turn (roll/yaw) input
 	turn_input = 0
 	turn_input -= Input.get_action_strength("roll_right")
@@ -49,21 +29,8 @@ func _physics_process(delta):
 	get_input(delta)
 	transform.basis = transform.basis.rotated(transform.basis.x, pitch_input * pitch_speed * delta)
 	transform.basis = transform.basis.rotated(Vector3.UP, turn_input * turn_speed * delta)
-	# If on the ground, don't roll the body
-	if grounded:
-		$Mesh/Body.rotation.z = 0
-	else:
-		$Mesh/Body.rotation.z = lerp($Mesh/Body.rotation.z, turn_input, level_speed * delta)
-	forward_speed = lerp(forward_speed, target_speed, acceleration * delta)
-	velocity = -transform.basis.z * forward_speed
-	# Handle landing/taking off
-	if is_on_floor():
-		if not grounded:
-			rotation.x = 0
-		velocity.y -= 1
-		grounded = true
-	else:
-		grounded = false
+	$Mesh/Body.rotation.z = lerp($Mesh/Body.rotation.z, turn_input, level_speed * delta)
+	velocity = -transform.basis.z * flight_speed
 
 	velocity = move_and_slide(velocity, Vector3.UP)
 
