68b364204c8c8cdd1e0a242798fd262203a71fa1
Valeron <32789691+Valeron-T@users.noreply.github.com>
2022-01-22 17:42:46 +0530

    Add files via upload

---
+++ game/Scripts/Universe.gd
@@ -0,0 +1,90 @@
+extends Spatial
+
+var scene = preload("res://Plane.tscn")
+
+#Saves game
+func save_game():
+	# I/O for save data
+	var save_game = File.new()
+	save_game.open("user://savegame.save", File.WRITE)
+	# Returns nodes which have properties to be saved
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	print(save_nodes)
+	# Iterates over nodes in Persist grp to check if it can be saved and if yes, calls the save function 
+	for node in save_nodes:
+		# Check the node is an instanced scene so it can be instanced again during load.
+		if node.filename.empty():
+			print("persistent node '%s' is not an instanced scene, skipped" % node.name)
+			continue
+
+		# Check the node has a save function.
+		if !node.has_method("save"):
+			print("persistent node '%s' is missing a save() function, skipped" % node.name)
+			continue
+
+		# Call the node's save function.
+		var node_data = node.call("save")
+		print("Saved Success")
+
+		# Store the save dictionary as a new line in the save file.
+		save_game.store_line(to_json(node_data))
+	save_game.close()
+
+#Loads saved game
+func load_game():
+	var save_game = File.new()
+	if not save_game.file_exists("user://savegame.save"):
+		return
+
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for i in save_nodes:
+		i.queue_free()
+
+	# Load the file line by line and process that dictionary to restore
+	# the object it represents.
+	save_game.open("user://savegame.save", File.READ)
+	while save_game.get_position() < save_game.get_len():
+		# Get the saved dictionary from the next line in the save file
+		var node_data = parse_json(save_game.get_line())
+
+		# Firstly, we need to create the object and add it to the tree and set its position.
+		var new_object = load(node_data["filename"]).instance()
+		get_node(node_data["parent"]).add_child(new_object)
+		new_object.global_transform.origin = Vector3(node_data["pos_x"],node_data["pos_y"],node_data["pos_z"])
+
+		for i in node_data.keys():
+			if i == "filename" or i == "parent" or i == "pos_x" or i == "pos_y":
+				continue
+			new_object.set(i, node_data[i])
+
+	save_game.close()
+
+
+#signal to alert that teleport animation has occured
+signal teleported
+signal teleportation_complete
+
+func _input(event):
+	if event.is_action_pressed("teleport"):
+		teleport()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+#function to play teleport animation
+func teleport():
+	$TeleportScreen/AnimationPlayer.play("into_teleport")
+
+#singal function to alert that teleport animation has finished
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "into_teleport":
+		emit_signal("teleported")
+		$TeleportScreen/AnimationPlayer.play("outof_teleport")
+		
+
+func _on_AnimationPlayer_animation_started(anim_name):
+	if anim_name == "outof_teleport":
+		emit_signal("teleportation_complete")
+		$base_world.visible = not $base_world.visible
+		$world2.visible = not $world2.visible
