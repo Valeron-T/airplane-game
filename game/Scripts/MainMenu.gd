f725872315b57b430468cd3e991493daf4c0ecb3
Valeron <32789691+Valeron-T@users.noreply.github.com>
2022-02-05 08:30:04 +0530

    Add files via upload

---
+++ game/Scripts/MainMenu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_PlayButton_pressed():
+	get_tree().change_scene("res://Universe.tscn")
